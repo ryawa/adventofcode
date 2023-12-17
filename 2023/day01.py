be7ab615862bc86192844b7f23eb2883912b6222
ryawa <22625346+ryawa@users.noreply.github.com>
2023-12-17 21:22:14 +0000

    Fix formatting

--- 2023/day01.py
+++ 2023/day01.py
@@ -16,15 +16,15 @@ print(ans)
 
 ans = 0
 numbers = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
 }
 for line in lines:
     found = []
